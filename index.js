7ccffe308a4ba96cccfb537febdd36a892925a8c
LuigiBelanda <luigi.belanda.milani@gmail.com>
2023-02-14 12:54:58 -0300

    Convertendo CSV para JSON / Salvando dados em JSON

--- index.js
+++ index.js
@@ -7,19 +7,51 @@
  *  https://www.w3schools.com/nodejs/met_path_dirname.asp
  *  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/import.meta
  *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
- *
+ *  https://httptoolkit.com/blog/unblocking-node-with-unref/
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+ *  https://www.w3schools.com/js/js_json_parse.asp
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+ *  https://www.w3schools.com/js/js_json_stringify.asp
+ * 
+ * 
+ *  Comandos usados:
+ *  npm start (node --harmony-top-level-await index.js)
+ *  npm start (DEBUG=app* --harmony-top-level-await index.js)
+ *  npm start | tee log.log
+ * 
+ *  Outros:
+ *  console.time("concat-data");
+ *  console.timeEnd("concat-data");
+ * 
+ *  Com os consoles acima conseguimos ver quanto tempo nosso código
+ *  demora para rodar tudo o que pedimos, no caso se reparar no código abaixo
+ *  encontrará o console.time("concat-data"); mais acima, várias linhas de código 
+ *  e lá no final o console.timeEnd("concat-data"); ou seja, o tempo que levar para rodar
+ *  o código que está no meio desses comandos vai ser exibido no final  
+ * 
  */
 
 // console.log(await Promise.resolve(true));
 
-import { dirname } from "node:path";
+import { dirname, join } from "node:path";
 import { promisify } from "node:util";
-
-import { promises } from "node:fs";
-const { readdir } = promises;
-
+import { promises, createReadStream, createWriteStream } from "node:fs";
+import { pipeline, Transform } from "node:stream";
 import debug from "debug";
+import csvtojson from "csvtojson";
+
+const { readdir } = promises;
 const log = debug("app:concat");
+const pipelineAsync = promisify(pipeline);
+
+/**
+ * 
+ *  PRIMEIRA ETAPA
+ *  Aqui garantimos os caminhos para os nossos arquivos
+ *  Vemos onde esta rodando nosso index
+ *  E localizamos nossa pasta com os arquivos csv etc
+ * 
+ */
 
 // pega o caminho da onde esta rodando o arquivo
 const { pathname: currentFile } = new URL(import.meta.url);
@@ -43,3 +75,65 @@ console.time("concat-data");
 const files = (await readdir(filesDir))
     .filter((item) => !!!~item.indexOf(".zip"));
 log(`Processing: ${files}`);
+
+/**
+ * 
+ *  SEGUNDA ETAPA:
+ *  Aqui fazemos uma pequena barra de progresso para ter algum sinal
+ *  para o user que o programa esta rodando e não está travado
+ * 
+ */
+
+// barra de progresso
+const ONE_SECOND = 1000;
+// quando os outros processos acabarem ele morre junto (unref)
+setInterval(() => process.stdout.write("."), ONE_SECOND).unref();
+
+/**
+ * 
+ *  TERCEIRA ETAPA:
+ *  Aqui começamos a trabalhar propriamente com as stream 
+ *  Lendo nosso arquivo (createReadStream / Readable Stream)
+ *  Transformando nossos dados de cvs para json ( csvtojson() )
+ *  Manipulando os dados e pegando apenas os dados que queremos (Transform / handleStream)
+ *  Por fim, enviando esses dados para alguma saída (createWriteStream / Writable stream)
+ * 
+ *  Essas etapas são definidas no nosso pipeline (pipelineAsync).
+ * 
+ *  Chunk = nossa dado / um pedaço do arquivo que foi lido
+ *  cb = callback / o que estamos retornando (1º param: error | 2º param: nosso dado) 
+ * 
+ */
+
+
+// lendo os arquivos (readable stream)
+const combinedStreams = createReadStream(join(filesDir, files[0]));
+
+// trabalhando com os dados dos arquivos - pegando apenas os dados que queremos
+const handleStream = new Transform({
+    transform: (chunk, enconding, cb) => {
+        const data = JSON.parse(chunk);
+
+        const output = {
+            id: data.Respondent,
+            country: data.Country
+        }
+
+        log(`Id: ${output.id}`);
+        return cb(null, JSON.stringify(output));
+    }
+})
+
+// escrevendo os dados (writable stream)
+const finalStream = createWriteStream(output);
+
+// etapas de execução da stream / pipeline
+await pipelineAsync(
+    combinedStreams,
+    csvtojson(),
+    handleStream,
+    finalStream
+);
+
+log(`${files.length} files merged! on ${output}`);
+console.timeEnd("concat-data");
